c44997eab47dd96c1f36cf04e3b753f8fe6f32ea
murakami <murakami@bitstar.jp>
2021-12-16 23:36:03 +0900

    IndexControllerおよびviewを作成。

--- laravel-app/app/Http/Controllers/IndexController.php
+++ laravel-app/app/Http/Controllers/IndexController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+    public function index ()
+    {
+        $hello = 'Hello,World!';
+        $hello_array = [
+            'Hello',
+            'こんにちは',
+            'Bonjour',
+        ];
+
+        return view('index', compact('hello', 'hello_array'));
+    }
 }
